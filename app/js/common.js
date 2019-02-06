82186afc04cca7b80ae9fce72e4c5d4caee89a09
alex <alex_kobilianskiy@bigmir.net>
2019-02-06 17:45:00 +0200

    main-page complete

--- app/js/common.js
+++ app/js/common.js
@@ -173,9 +173,10 @@ $(document).ready(function(){
 
                 if(videoPlayers[i])
                 {
-                    var fn = function(){ videoPlayers[i].playVideo(); }
-                    setTimeout(fn, 1000);
+                    var fn = function(){ videoPlayers[i].playVideo(); };
+                    setTimeout(fn, 1500);
                 }
+
             });
             i++;
         });
@@ -195,37 +196,269 @@ $(document).ready(function(){
      * end YOUTUBE SCRIPT
      */
 
-    $('img.svg').each(function(){
-        var $img = jQuery(this);
-        var imgID = $img.attr('id');
-        var imgClass = $img.attr('class');
-        var imgURL = $img.attr('src');
+    // $('img.svg').each(function(){
+    //     var $img = jQuery(this);
+    //     var imgID = $img.attr('id');
+    //     var imgClass = $img.attr('class');
+    //     var imgURL = $img.attr('src');
+    //
+    //     jQuery.get(imgURL, function(data) {
+    //         // Get the SVG tag, ignore the rest
+    //         var $svg = jQuery(data).find('svg');
+    //
+    //         // Add replaced image's ID to the new SVG
+    //         if(typeof imgID !== 'undefined') {
+    //             $svg = $svg.attr('id', imgID);
+    //         }
+    //         // Add replaced image's classes to the new SVG
+    //         if(typeof imgClass !== 'undefined') {
+    //             $svg = $svg.attr('class', imgClass+' replaced-svg');
+    //         }
+    //
+    //         // Remove any invalid XML tags as per http://validator.w3.org
+    //         $svg = $svg.removeAttr('xmlns:a');
+    //
+    //         // Check if the viewport is set, if the viewport is not set the SVG wont't scale.
+    //         if(!$svg.attr('viewBox') && $svg.attr('height') && $svg.attr('width')) {
+    //             $svg.attr('viewBox', '0 0 ' + $svg.attr('height') + ' ' + $svg.attr('width'))
+    //         }
+    //
+    //         // Replace image with new SVG
+    //         $img.replaceWith($svg);
+    //     }, 'xml');
+    // });
 
-        jQuery.get(imgURL, function(data) {
-            // Get the SVG tag, ignore the rest
-            var $svg = jQuery(data).find('svg');
 
-            // Add replaced image's ID to the new SVG
-            if(typeof imgID !== 'undefined') {
-                $svg = $svg.attr('id', imgID);
+    /**
+     * YA-MAPS
+     */
+
+    //Переменная для включения/отключения индикатора загрузки
+    var spinner = $('.loader');
+    //Переменная для определения была ли хоть раз загружена Яндекс.Карта (чтобы избежать повторной загрузки при наведении)
+    var check_if_load = false;
+    //Необходимые переменные для того, чтобы задать координаты на Яндекс.Карте
+    //var myMapTemp, myPlacemarkTemp;
+
+
+    //Функция создания карты сайта и затем вставки ее в блок с идентификатором &#34;map-yandex&#34;
+    function init () {
+        var mapId = $('#map'),
+            attitude = mapId.data("att"),
+            longtitude = mapId.data("long"),
+            zoom = mapId.data("zoom"),
+            marker = mapId.data("marker"),
+            map = new ymaps.Map("map", {
+                center: [attitude, longtitude],
+                controls: ['zoomControl'],
+                zoom: zoom
+            }),
+
+            myPlacemark = new ymaps.Placemark(map.getCenter(), {}, {
+                // Опции.
+                // Необходимо указать данный тип макета.
+                iconLayout: 'default#image',
+                // Своё изображение иконки метки.
+                iconImageHref: marker,
+                // Размеры метки.
+                iconImageSize: [32.768, 43],
+                // Смещение левого верхнего угла иконки относительно
+                // её "ножки" (точки привязки).
+                iconImageOffset: [-16, -40]
+            });
+
+        map.geoObjects.add(myPlacemark);
+        map.behaviors.disable('scrollZoom');
+
+
+        var position = map.getGlobalPixelCenter();
+
+        map.setGlobalPixelCenter([ position[0] - 350, position[1] ]);
+
+        if ($(window).width() <= 1500) {
+            map.setGlobalPixelCenter([ position[0] - 250, position[1]]);
+        }
+
+        if ($(window).width() < 992) {
+            map.setGlobalPixelCenter([ position[0]-200, position[1]]);
+        }
+
+        if ($(window).width() < 767) {
+            map.setGlobalPixelCenter([ position[0], position[1] - 200]);
+        }
+
+
+        if ($(window).width() < 480) {
+            map.behaviors.disable('drag');
+            map.setGlobalPixelCenter([ position[0], position[1] - 150]);
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        // var myMapTemp = new ymaps.Map("map", {
+        //     center: [55.730138, 37.594238], // координаты центра на карте
+        //     zoom: 7, // коэффициент приближения карты
+        //     controls: ['zoomControl', 'fullscreenControl'] // выбираем только те функции, которые необходимы при использовании
+        // });
+        // var myPlacemarkTemp = new ymaps.GeoObject({
+        //     geometry: {
+        //         type: "Point",
+        //         coordinates: [55.730138, 37.594238] // координаты, где будет размещаться флажок на карте
+        //     }
+        // });
+        // myMapTemp.geoObjects.add(myPlacemarkTemp); // помещаем флажок на карту
+
+        // Получаем первый экземпляр коллекции слоев, потом первый слой коллекции
+        var layer = map.layers.get(0).get(0);
+
+        // Решение по callback-у для определения полной загрузки карты
+        waitForTilesLoad(layer).then(function() {
+            // Скрываем индикатор загрузки после полной загрузки карты
+            spinner.removeClass('is-active');
+        });
+    }
+
+
+    // Функция для определения полной загрузки карты (на самом деле проверяется загрузка тайлов)
+    function waitForTilesLoad(layer) {
+        return new ymaps.vow.Promise(function (resolve, reject) {
+            var tc = getTileContainer(layer), readyAll = true;
+            tc.tiles.each(function (tile, number) {
+                if (!tile.isReady()) {
+                    readyAll = false;
+                }
+            });
+            if (readyAll) {
+                resolve();
+            } else {
+                tc.events.once("ready", function() {
+                    resolve();
+                });
             }
-            // Add replaced image's classes to the new SVG
-            if(typeof imgClass !== 'undefined') {
-                $svg = $svg.attr('class', imgClass+' replaced-svg');
+        });
+    }
+
+    function getTileContainer(layer) {
+        for (var k in layer) {
+            if (layer.hasOwnProperty(k)) {
+                if (
+                    layer[k] instanceof ymaps.layer.tileContainer.CanvasContainer
+                    || layer[k] instanceof ymaps.layer.tileContainer.DomContainer
+                ) {
+                    return layer[k];
+                }
             }
+        }
+        return null;
+    }
 
-            // Remove any invalid XML tags as per http://validator.w3.org
-            $svg = $svg.removeAttr('xmlns:a');
 
-            // Check if the viewport is set, if the viewport is not set the SVG wont't scale.
-            if(!$svg.attr('viewBox') && $svg.attr('height') && $svg.attr('width')) {
-                $svg.attr('viewBox', '0 0 ' + $svg.attr('height') + ' ' + $svg.attr('width'))
+
+
+
+
+
+
+// Функция загрузки API Яндекс.Карт по требованию (в нашем случае при наведении)
+    function loadScript(url, callback){
+        var script = document.createElement("script");
+
+        if (script.readyState){  // IE
+            script.onreadystatechange = function(){
+                if (script.readyState == "loaded" ||
+                    script.readyState == "complete"){
+                    script.onreadystatechange = null;
+                    callback();
+                }
+            };
+        } else {  // Другие браузеры
+            script.onload = function(){
+                callback();
+            };
+        }
+
+        script.src = url;
+        document.getElementsByTagName("head")[0].appendChild(script);
+    }
+
+// Основная функция, которая проверяет когда мы навели на блок с классом &#34;ymap-container&#34;
+    var ymap = function() {
+        $('.s-map').on( "mouseenter", function(){
+            if (!check_if_load) { // проверяем первый ли раз загружается Яндекс.Карта, если да, то загружаем
+
+                // Чтобы не было повторной загрузки карты, мы изменяем значение переменной
+                check_if_load = true;
+
+                // Показываем индикатор загрузки до тех пор, пока карта не загрузится
+                spinner.addClass('is-active');
+
+                // Загружаем API Яндекс.Карт
+                loadScript("https://api-maps.yandex.ru/2.1/?apikey=e470b388-a1d0-4edf-acdc-34b4bc5bedee&lang=ru_RU&loadByRequire=1", function(){
+                    // Как только API Яндекс.Карт загрузились, сразу формируем карту и помещаем в блок с идентификатором &#34;map-yandex&#34;
+                    ymaps.load(init);
+                });
             }
+        });
+    }
 
-            // Replace image with new SVG
-            $img.replaceWith($svg);
-        }, 'xml');
-    });
+    ymap();
+
+
+    //
+    // $(function() {
+    //
+    //     //Запускаем основную функцию
+    //     //ymap();
+    //
+    // });
+    //
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // ymaps.ready(function(){
+    //
+    // });
+    /**
+     * end YA-MAPS
+     */
+
+
 
     //E-mail Ajax Send
     $("form").submit(function() { //Change
